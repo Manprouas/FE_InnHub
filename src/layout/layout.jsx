e55165a0bb7e6a5f4be3de65467f3d613fba08a9
pharaohpants <rantauhimawan@gmail.com>
2024-11-15 21:32:40 +0700

    Penambahan Sign up dan Login

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,17 +1,23 @@
-import Footer from "../component/Footer"
-import Header from "../component/Header"
-import Routers from "../routes/Routes"
+import { useLocation } from 'react-router-dom';
+import Footer from "../component/Footer";
+import Header from "../component/Header";
+import Routers from "../routes/Routes";
 
 const Layout = () => {
+    const location = useLocation();
+
+    // Cek apakah path saat ini adalah "/login" atau "/signup"
+    const isAuthPage = location.pathname === "/Login" || location.pathname === "/Signup";
+
     return (
         <>
-            <Header/>
+            {!isAuthPage && <Header />}
             <main>
-                <Routers/>
+                <Routers />
             </main>
-            <Footer/>
+            {!isAuthPage && <Footer />}
         </>
-    )
+    );
 }
 
-export default Layout
+export default Layout;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
 
 const Routers = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path=''/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
         <Route/>
       </Routes>
     </div>
