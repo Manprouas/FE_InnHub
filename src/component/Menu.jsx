623f12f616daf21b08bbf3545ea4c3e3533564f9
pharaohpants <rantauhimawan@gmail.com>
2024-11-29 03:40:11 +0700

    Perbaikan Menu

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -7,34 +7,49 @@ function Menu() {
     const [isOpen, setIsOpen] = useState(true);
 
     const menuItems = [
-        { name: 'Dashboard', icon: <FaTachometerAlt />, key: 'dashboard' },
-        { name: 'Reservations', icon: <FaCalendarCheck />, key: 'reservations' },
-        { name: 'Customers', icon: <FaUsers />, key: 'customers' },
-        { name: 'Rooms', icon: <FaBed />, key: 'rooms' },
-        { name: 'Reports', icon: <FaChartLine />, key: 'reports' },
-        { name: 'Settings', icon: <FaCog />, key: 'settings' },
+        { name: 'Home', icon: <FaTachometerAlt />, key: 'home' },
+        { name: 'Transactions', icon: <FaCalendarCheck />, key: 'transactions' },
+        { name: 'Users', icon: <FaUsers />, key: 'users' },
+        { name: 'Hotels', icon: <FaBed />, key: 'hotels' },
+        { name: 'Complaint', icon: <FaChartLine />, key: 'complaint' },
+    ];
+
+    const adminItems = [
+        { name: 'Admin', icon: <FaCog />, key: 'admin' },
     ];
 
     return (
         <div className="flex h-screen mt-6 bg-white">
-            <div className={`rounded-3xl bg-white text-blue-600 drop-shadow-xl ${isOpen ? 'w-64' : 'w-16'} transition-width duration-300`}>
+            <div className={`rounded-3xl bg-white text-slate-950 drop-shadow-xl ${isOpen ? 'w-64' : 'w-16'} h-auto transition-width duration-300`}>
                 <div className="p-4 text-center text-xl font-bold border-b border-skyblue-600 flex justify-between items-center">
-                    {isOpen && <span className='text-blue-700 drop-shadow-2xl'>InnHub</span>}
+                    {isOpen && <img src='src\assets\logo1.png'></img>}
                     <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
                         <FaBars />
                     </button>
                 </div>
-                <ul className="mt-4">
-                    {menuItems.map(item => (
-                        <li key={item.key} className={`p-4 cursor-pointer flex items-center ${active === item.key ? 'bg-blue-800 text-white' : 'hover:bg-blue-700 hover:text-white'} transition-colors duration-200`} onClick={() => setActive(item.key)}>
-                            <div className={`${isOpen ? 'mr-3' : 'mx-auto'}`}>{item.icon}</div>
-                            {isOpen && item.name}
-                        </li>
-                    ))}
+                <ul className="mt-4 flex flex-col justify-between h-full">
+                    <div>
+                        {menuItems.map(item => (
+                            <li key={item.key} className={`p-4 cursor-pointer flex items-center m-2  ${active === item.key ? 'bg-yellow-500 text-white rounded-3xl m-2' : 'hover:bg-yellow-500 hover:text-white rounded-3xl'} transition-colors duration-200`} onClick={() => setActive(item.key)}>
+                                <div className={`${isOpen ? 'mr-3' : 'mx-auto'}`}>{item.icon}</div>
+                                {isOpen && item.name}
+                            </li>
+                        ))}
+                    </div>
+                    <div className="mt-auto mb-28">
+                        <ul>
+                            {adminItems.map(item => (
+                                <li key={item.key} className={`p-4 cursor-pointer flex items-center m-2 ${active === item.key ? 'bg-yellow-500 text-white rounded-3xl m-2' : 'hover:bg-yellow-500 hover:text-white rounded-3xl'} transition-colors duration-200`} onClick={() => setActive(item.key)}>
+                                    <div className={`${isOpen ? 'mr-3' : 'mx-auto'}`}>{item.icon}</div>
+                                    {isOpen && item.name}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </ul>
             </div>
             <div className="flex-1 p-6">
-            <Hero/>
+                <Hero />
             </div>
         </div>
     );
